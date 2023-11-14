a2ad8636d5be18e182bb750dfd11787441a9628e
ethanJcoding <ethan.95j@gmail.com>
2023-11-14 13:42:25 +0900

    Feat: 참여한 내전에 취소하는 command 생성

---
+++ firebase/cancelParticipation/cancelParticipation.js
@@ -0,0 +1,25 @@
+import { database } from "../../bot.js";
+import { ref, get, update } from "firebase/database";
+
+const cancelParticipation = async (gameId, guildId, username) => {
+  try {
+    const gameRef = ref(database, `guilds/${guildId}/games/${gameId}`);
+    const snapshot = await get(gameRef);
+
+    if (snapshot.exists()) {
+      const game = snapshot.val();
+
+      const updatedMembers = game.members.filter(
+        member => member.user !== username
+      );
+
+      await update(gameRef, { members: updatedMembers });
+      return "취소 성공 😎";
+    }
+  } catch (error) {
+    console.error("Error while fetching active games:", error);
+    return [];
+  }
+};
+
+export { cancelParticipation };
